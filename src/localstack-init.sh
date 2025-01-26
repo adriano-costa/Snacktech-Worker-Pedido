da58f722b2ecad1428142a15bab4374d2c69f712
Adriano Costa <adriano.dmcosta@gmail.com>
2025-01-26 15:23:03 -0300

    Usar localstack para subir um serviço local para testar filas e usar um container sql server para o banco

---
+++ src/localstack-init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "Running LocalStack initialization..."
+
+awslocal sqs create-queue --queue-name atualizar-pedido.fifo --attributes 'FifoQueue=true, ContentBasedDeduplication=true' 
+awslocal sqs create-queue --queue-name atualizar-pedido-dlq
